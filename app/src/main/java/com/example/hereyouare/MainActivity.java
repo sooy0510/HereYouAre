ef1acec23873bafb3bd9910dd65d391f00c583d0
sooy0510 <qkdqnwpwp@naver.com>
2019-11-22 17:59:17 +0900

    191122 | Status Bar 변경

--- app/src/main/java/com/example/hereyouare/MainActivity.java
+++ app/src/main/java/com/example/hereyouare/MainActivity.java
@@ -15,6 +15,7 @@ import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     Button btn;
+    Button s1;
     Button quickBtn;
 
     /* Quick Action */
@@ -28,42 +29,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btn = (Button)findViewById(R.id.btn1);
+
+        s1 = (Button)findViewById(R.id.s1);
+        btn = s1;
         btn.setAlpha(0);
         btn.setOnClickListener(this);
 
-//        /* Quick Action */
-//        infoItem 	= new ActionItem(ID_INFO, "title1", "title2", getResources().getDrawable(R.drawable.menu_info));
-//        quickAction = new QuickAction(this, QuickAction.VERTICAL);
-//
-//        //add action items into QuickAction
-//        quickAction.addActionItem(infoItem);
-//
-//        //Set listener for action item clicked
-//        quickAction.setOnActionItemClickListener(new QuickAction.OnActionItemClickListener() {
-//            @Override
-//            public void onItemClick(QuickAction source, int pos, int actionId) {
-//                ActionItem actionItem = quickAction.getActionItem(pos);
-//
-//                //here we can filter which action item was clicked with pos or actionId parameter
-//                if (actionId == ID_SEARCH) {
-//                    Toast.makeText(getApplicationContext(), "Let's do some search action", Toast.LENGTH_SHORT).show();
-//                } else if (actionId == ID_INFO) {
-//                    Toast.makeText(getApplicationContext(), "I have no info this time", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    Toast.makeText(getApplicationContext(), actionItem.getAsc_title() + " selected", Toast.LENGTH_SHORT).show();
-//                }
-//            }
-//        });
-//
-//        //set listnener for on dismiss event, this listener will be called only if QuickAction dialog was dismissed
-//        //by clicking the area outside the dialog.
-//        quickAction.setOnDismissListener(new QuickAction.OnDismissListener() {
-//            @Override
-//            public void onDismiss() {
-//                Toast.makeText(getApplicationContext(), "Dismissed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+        /*button 모두 alpha(0)으로 만드는 코드
+        TableLayout tableLayoutInstance; // let's suppouse you have already initialized it
+// blablabla
+// example to deactivate all buttons
+ArrayList<View> touchables = tableLayoutInstance.getTouchables();
+for(View touchable : touchables){
+    if( touchable instanceof Button )
+        ((Button)touchable).setEnabled(false);
+}
+        * */
+
 
     }
 
@@ -72,7 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Station 선택
         switch (view.getId()){
-            case R.id.btn1:
+            case R.id.s1:
                 btn.setAlpha(1);
                 Toast.makeText(this, "나와랏", Toast.LENGTH_SHORT).show();
                 //ArrayList<HashMap> jsonList = new ArrayList<>(); //return 해줄 route 정보들
@@ -138,6 +120,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         quickAction.setOnDismissListener(new QuickAction.OnDismissListener() {
             @Override
             public void onDismiss() {
+                btn.setAlpha(0);
                 Toast.makeText(getApplicationContext(), "Dismissed", Toast.LENGTH_SHORT).show();
             }
         });
